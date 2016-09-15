4b7d05ee35c8fc3c47447912bf7507f157cf6c20
Chapman Siu <chpmn.siu@gmail.com>
2016-09-15 16:41:24 +1000

    it works!

--- create-repo.py
+++ create-repo.py
@@ -12,3 +12,23 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 # https://tryolabs.com/blog/2015/02/17/python-elasticsearch-first-steps/
 # pump csv file into elasticsearch repo...
+
+with open('omscs-compiled-faq', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for idx, row in enumerate(csvreader):
+        if idx == 0: continue
+        es.index(index='faq', doc_type='faq-type', id=idx, body={
+            'question': row[0],
+            'answer': row[1],
+            'source': row[2]
+        })
+
+
+# try a search 
+query = es.search(index="faq", body={"query": {"match": {'question':'How do I register?'}}})
+
+import pprint
+
+pprint.pprint(query['hits']['hits'])
+
+
