b10aaea51ebaf03d05cd16799c3f832398da0f39
Chapman Siu <chpmn.siu@gmail.com>
2016-10-19 22:17:27 +1100

    lda online
    updated some more data from sese...

--- data-prep.py
+++ data-prep.py
@@ -14,7 +14,7 @@ def collapse_spaces(x):
     return x.strip()
 
 # clean up body 
-so_dat = pd.read_csv("SESE/sql-html-js-1.csv", nrows=10000)
+so_dat = pd.read_csv("SESE/sql-html-js-1.csv", nrows=50000)
 
 so_dat_main = so_dat[['id', 'title', 'body', 'tags']]
 so_dat_main['body'] = pd.Series([collapse_spaces(h.handle(x)) for x in so_dat_main['body'].tolist()])
@@ -23,17 +23,25 @@ so_dat_main['body'] = pd.Series([collapse_spaces(h.handle(x)) for x in so_dat_ma
 
 import gensim
 from gensim.parsing import stem_text
-from gensim.utils import smart_open, simple_preprocess
+from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 
 def tokenize(text):
     return [stem_text(token) for token in simple_preprocess(text) if token not in STOPWORDS]
 
-texts = [tokenize(x) for x in so_dat_main[['body']].tolist()]
+texts = [tokenize(x) for x in so_dat_main['body'].tolist()]
 dictionary = gensim.corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 # 100 was used in the stackoverflow paper...
 lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=100, update_every=1, chunksize=10000, passes=1)
 
-# lda.print_topics(5)
+lda.print_topics(5)
+
+# try updating the model...
+so_dat = pd.read_csv("SESE/sql-html-js-2.csv", nrows=10000)
+so_dat_main = so_dat[['id', 'title', 'body', 'tags']]
+texts = [tokenize(x) for x in so_dat_main['body'].tolist()]
+dictionary = gensim.corpora.Dictionary(texts)
+new_corpus = [dictionary.doc2bow(text) for text in texts]
+lda.update(new_corpus)
