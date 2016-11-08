88cbd4c9909ee6d2f29bd58323e2756b7a4d2388
Chapman Siu <chpmn.siu@gmail.com>
2016-11-08 23:01:26 +1100

    label code

---
+++ feature-engineering/create_labels.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+for fname in [
+    "../SESE/sql-html-js-2",
+    "../SESE/sql-html-js-1",
+    "../SESE/sql-html-js-3",
+    "../SESE/sql-html-js-4",
+    "../SESE/sql-html-js-5"
+    ]:
+    so_dat = pd.read_csv(fname+".csv")
+    so_dat_main = so_dat[['id', 'did']]
+    so_dat_main = so_dat_main.assign(label = np.where(np.isnan(so_dat_main['did']), 0, 1))
+    so_dat_main[['id', 'label']].to_csv(fname+"_labels.csv")
+
+
+    
+    
